73a7c4fe49020d34ca4b93a57ac439183cf3885e
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-05-01 19:43:06 -0300

    Improving test report.

--- elisp/emidje.el
+++ elisp/emidje.el
@@ -13,7 +13,7 @@
   :group 'midje
   :package-version '(emidje . "0.1.0"))
 
-(defconst emidje-nrepl-version "0.1.0-SNAPSHOT")
+(defconst midje-nrepl-version "0.1.0-SNAPSHOT")
 
 (defun emidje-inject-jack-in-dependencies ()
   (add-to-list 'cider-jack-in-lein-plugins `("midje-nrepl" ,midje-nrepl-version) t))
@@ -23,7 +23,41 @@
   '(emidje-inject-jack-in-dependencies))
 
 (defun emidje-render-one-test-result (result)
-  (print result))
+  (nrepl-dbind-response result (context expected actual error message type)
+    (cl-flet ((insert-label (s)
+                            (cider-insert (format "%8s: " s) 'font-lock-comment-face))
+              (insert-align-label (s)
+                                  (insert (format "%12s" s)))
+              (insert-rect (s)
+                           (insert-rectangle (thread-first s
+                                               cider-font-lock-as-clojure
+                                               (split-string "\n")))
+                           (beginning-of-line)))
+      (cider-propertize-region (cider-intern-keys (cdr result))
+        (let ((beg (point))
+              (type-face (cider-test-type-simple-face type))
+              (bg `(:background ,cider-test-items-background-color)))
+          (cider-insert (capitalize type) type-face nil " in ")
+          (dolist (text context)
+            (cider-insert text 'font-lock-doc-face t))
+          (when expected
+            (insert-label "expected")
+            (insert-rect expected)
+            (insert "\n"))
+          (when actual
+            (insert-label "actual")
+            (insert-rect actual))
+          (unless (seq-empty-p message)
+            (dolist (text message)
+              (cider-insert text 'font-lock-doc-string-face t)))
+          (when error
+            (insert-label "error")
+            (insert-text-button error
+                                'follow-link t
+                                'action '(lambda (_button) (cider-test-stacktrace))
+                                'help-echo "View causes and stacktrace"))
+          (overlay-put (make-overlay beg (point)) 'font-lock-face bg))
+        (insert "\n\n")))))
 
 (defun emidje-count-non-passing-tests (results)
   (seq-count (lambda (result)
@@ -31,20 +65,27 @@
                  (and (not (equal type "error"))
                       (not (equal type "fail"))))) results))
 
+(defun emidje-get-displayable-results (results)
+  (seq-filter (lambda (result)
+                (not (equal (nrepl-dict-get result "type") "pass")))
+              results))
+
 (defun emidje-render-test-results (results-dict)
   (cider-insert "Results" 'bold t "\n")
   (nrepl-dict-map (lambda (ns results)
-                    (let* ((problems (emidje-count-non-passing-tests results)))
+                    (let* ((displayable-results (emidje-get-displayable-results results))
+                           (problems (emidje-count-non-passing-tests displayable-results)))
                       (when (> problems 0)
                         (insert (format "%s\n%d non-passing tests:\n\n"
-                                        (cider-propertize ns 'ns) problems))))
-                    (dolist (result results)
-                      (emidje-render-one-test-result result))
+                                        (cider-propertize ns 'ns) problems)))
+                      (dolist (result displayable-results)
+                        (emidje-render-one-test-result result)))
                     ) results-dict))
 
 (defun emidje-render-list-of-namespaces (results)
   (dolist (namespace (nrepl-dict-keys results))
-    (insert (cider-propertize namespace 'ns) "\n")))
+    (insert (cider-propertize namespace 'ns) "\n")
+    (insert "\n")))
 
 (defun emidje-render-test-summary (summary)
   (nrepl-dbind-response summary (error fact fail ns pass test skip)
@@ -57,7 +98,8 @@
     (unless (zerop skip)
       (cider-insert (format "%d to do" skip) 'midje-work-todo-face t))
     (when (zerop (+ fail error))
-      (cider-insert (format "%d passed" pass) 'cider-test-success-face t))))
+      (cider-insert (format "%d passed" pass) 'cider-test-success-face t))
+    (insert "\n")))
 
 (defun emidje-get-test-report-buffer ()
   (or (get-buffer cider-test-report-buffer)
