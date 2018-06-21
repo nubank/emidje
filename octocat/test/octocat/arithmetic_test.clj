398107b10826d459772ad2c83f5bd4516d072ad9
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-06-21 18:01:16 -0300

    Include a tabular fact for testing purposes.

--- octocat/test/octocat/arithmetic_test.clj
+++ octocat/test/octocat/arithmetic_test.clj
@@ -6,11 +6,21 @@
        (fact (* 2 5) => 10)
 
        (fact "this is a crazy arithmetic"
-             (+ 2 3) => 6)
+             (+ 2 3) => {:a 1})
 
        (fact "two assertions in the same fact; the former is correct while the later is wrong"
              (+ 10 1) => 11
              (- 4 2) => 3)
 
        (fact "this will throw an unexpected exception"
-             (/ 12 0) => 0))
+             (/ 12 0) => 0)
+
+       (tabular (fact "about basic arithmetic operations"
+                      (?operation ?a ?b) => ?result)
+                ?operation ?a ?b ?result
+                + 2 5 10
+                + 10 4 14
+                - 100 25 75
+                * 123 69 8487
+                / 15 8 15/8
+                / 4284 126 34))
