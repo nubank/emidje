ad9bd75b3cf4e9d2ecd7bfd401ff703f50738312
Alan Ghelardi <alan.ghelardi@gmail.com>
2018-11-10 14:50:08 -0200

    Add seq to the list of required packages.

--- emidje.el
+++ emidje.el
@@ -3,7 +3,7 @@
 ;; Author: Alan Ghelardi <alan.ghelardi@nubank.com.br>
 ;; Maintainer: Alan Ghelardi <alan.ghelardi@nubank.com.br>
 ;; Version: 1.0.0
-;; Package-Requires: ((emacs "25") (cider "0.17.0"))
+;; Package-Requires: ((emacs "25") (cider "0.17.0") (seq "2.16"))
 ;; Homepage: https://github.com/nubank/emidje
 ;; Keywords: tools
 
