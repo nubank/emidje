77506abec4ce81fef169e1e551c4e4666f65072c
Alan Ghelardi <alan.ghelardi@gmail.com>
2018-12-01 12:44:12 -0200

    Include license notice.

--- emidje.el
+++ emidje.el
@@ -1,12 +1,25 @@
 ;;; emidje.el --- Test runner and report viewer for Midje -*- lexical-binding: t -*-
 
 ;; Copyright © 2018 Nubank
+
 ;; Author: Alan Ghelardi <alan.ghelardi@nubank.com.br>
 ;; Maintainer: Alan Ghelardi <alan.ghelardi@nubank.com.br>
 ;; Version: 1.0.1-SNAPSHOT
 ;; Package-Requires: ((emacs "25") (cider "0.17.0") (seq "2.16"))
 ;; Homepage: https://github.com/nubank/emidje
 ;; Keywords: tools
+
+;; This file is not part of GNU Emacs.
+
+;; Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+;; this file except in compliance with the License.  You may obtain a copy of the
+;; License at
+;; http://www.apache.org/licenses/LICENSE-2.0
+
+;; Unless required by applicable law or agreed to in writing, software distributed
+;; under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+;; CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+;; specific language governing permissions and limitations under the License.
 
 ;;; Commentary:
 
