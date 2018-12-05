9dd09f8eadf9c235a71882c1aa48907afc0808e4
Alan Ghelardi <alan.ghelardi@gmail.com>
2018-12-05 18:53:51 -0200

    Update references.

--- bin/release.sh
+++ bin/release.sh
@@ -4,14 +4,24 @@ set -euo pipefail
 version=$1
 
 update_changelog() {
-    changelog=CHANGELOG.md
+    update_unreleased_section
+    update_reference_links
+    rm -f ${changelog}e
+}
+
+update_unreleased_section() {
     today=$(date --utc +'%Y-%m-%d')
     sed -ie "s/\(##\s*\[Unreleased\]\)/\1\n\n## [$version] - $today/g" $changelog
-    git add $changelog
+}
+
+update_reference_links() {
+    local unreleased_ref="[Unreleased]: https:\/\/github.com\/nubank\/emidje\/compare\/$version...HEAD"
+    sed -ie "s/\[Unreleased\]\(:.*\)HEAD$/$unreleased_ref\n[$version]\1$version/g" $changelog
 }
 
 commit_and_push() {
     local current_branch=$(git symbolic-ref HEAD | sed 's!refs\/heads\/!!')
+    git add $changelog
     git commit -m "Release version $version"
     git push origin $current_branch
 }
